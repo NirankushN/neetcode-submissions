7291485ad4dcce6573f0d46baab412d971d5d5ed
NirankushN <61386731+NirankushN@users.noreply.github.com>
2026-04-17 22:40:03 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int[] ele=new int[45+1];
+    public int countStairs(int n){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(ele[n]!=0){
+            return ele[n];
+        }
+        ele[n]=countStairs(n-1)+countStairs(n-2);
+        return ele[n];
+    }
+    public int climbStairs(int n) {
+        ele[1]=1;
+        ele[2]=2;
+        ele[n]=countStairs(n);
+        return ele[n];
+    }
+}
